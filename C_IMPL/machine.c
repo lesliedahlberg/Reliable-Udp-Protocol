e21937170af5814f606a55846abdbc61c1a2c287
jontelarsson94 <jonathan-larsson@outlook.com>
2016-02-04 12:35:01 +0100

    -

--- C_IMPL/machine.c
+++ C_IMPL/machine.c
@@ -152,15 +152,21 @@ void send_packet(char data[32]){
 
 }
 
-/* SEND PACKET */
+/* SEND ACK */
 void send_ack(int seq){
   PACKET ack_packet;
+
   ack_packet.ack = 1;
   ack_packet.seq = seq;
+  unsigned int slen = sizeof(server_socket_client_address);
   //SEND over network
+  if(sendto(server_socket, &ack_packet, sizeof(PACKET), 0, (struct sockaddr *) &server_socket_client_address, &slen)==-1)
+    {
+        //die("sendto()");
+        perror("Sendto.\n");
+        exit(EXIT_FAILURE);
+    }
   printf("SEND ACK: seq: %d;\n", ack_packet.seq);
-
-
 }
 
 /* RECIEVE PACKET */

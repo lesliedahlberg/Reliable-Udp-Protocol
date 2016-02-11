aeba5f5c022dba5603587fa174bfe1249ccdbe24
jontelarsson94 <jonathan-larsson@outlook.com>
2016-02-11 14:40:56 +0100

    -

--- C_IMPL/FINAL/transport.c
+++ C_IMPL/FINAL/transport.c
@@ -26,7 +26,7 @@
          /* No acks sent yet */
          first_ack = 1;
          re_ack = 0;
-         drop_rate = 4;
+         drop_rate = 1000000000;
 
          /* Buffer setup*/
          server_buf.seq_0 = 0; //Not used
